970f9ff5b98685b9ba7fa5370dd17d9a03e7404f
theyusa <108272816+theyusa@users.noreply.github.com>
2026-01-07 22:12:20 +0300

    Update ExpandableLayout.kt

--- app/src/main/java/com/neko/expandable/layout/ExpandableLayout.kt
+++ app/src/main/java/com/neko/expandable/layout/ExpandableLayout.kt
@@ -2,6 +2,7 @@ package com.neko.expandable.layout
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.graphics.Color
 import android.graphics.drawable.RippleDrawable
 import android.util.AttributeSet
 import android.view.View
@@ -21,7 +22,6 @@ class ExpandableLayout(context: Context, attrs: AttributeSet?) : LinearLayout(co
         arrowIcon = findViewById(R.id.arrow_button)
         cardExpandable = findViewById(R.id.card_expandable)
         
-        [span_7](start_span)// Hem karta hem oka tıklama özelliği[span_7](end_span)
         cardExpandable.setOnClickListener(this)
         arrowIcon.setOnClickListener(this)
         
@@ -33,28 +33,23 @@ class ExpandableLayout(context: Context, attrs: AttributeSet?) : LinearLayout(co
     }
 
     private fun toggleExpansion() {
-        // Her tıklamada yönün dikey olduğundan emin oluyoruz (eski kodun kararlılığı için)
         expandableContent.setOrientation(ExpandableView.VERTICAL)
-        
         val isExpanding = !expandableContent.isExpanded
         
         if (isExpanding) {
             expandableContent.expand()
-            [span_8](start_span)// Ok ikonunu aşağı döndür (90 derece)[span_8](end_span)
             arrowIcon.animate().setDuration(300L).rotation(90.0f).start()
         } else {
             expandableContent.collapse()
-            [span_9](start_span)// Ok ikonunu yana döndür (0 derece)[span_9](end_span)
             arrowIcon.animate().setDuration(300L).rotation(0.0f).start()
         }
     }
 
     private fun initializeLogic() {
-        [span_10](start_span)// Ripple efekti ve başlangıç durumu[span_10](end_span)
-        arrowIcon.background = RippleDrawable(ColorStateList(arrayOf(intArrayOf()), intArrayOf(-0x8a8a8b)), null, null)
+        val rippleColor = ColorStateList.valueOf(Color.parseColor("#448A8A8B"))
+        arrowIcon.background = RippleDrawable(rippleColor, null, null)
         arrowIcon.isClickable = true
         
-        [span_11](start_span)// Başlangıçta açıksa oku 90 derece yap[span_11](end_span)
         if (expandableContent.isExpanded) {
             arrowIcon.rotation = 90.0f
         } else {
